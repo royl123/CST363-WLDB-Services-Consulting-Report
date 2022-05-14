089a51cc1e7e365a151bdf02a8ed264f2ea4c979
Roy Luengas <royluengas1@gmail.com>
2022-05-14 14:51:29 -0400

    Application.java add example insert row

--- src/main/java/com/csumb/cst363/Cst363Application.java
+++ src/main/java/com/csumb/cst363/Cst363Application.java
@@ -1,13 +1,29 @@
 package com.csumb.cst363;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Cst363Application {
-
+public class Cst363Application implements CommandLineRunner {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cst363Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO doctor(ssn, name, specialty, year_experience) VALUES (?,?,?,?)";
+		int result = jdbcTemplate.update(sql, "987654321", "Robert Jackson", "cardiology", "20");
+		
+		if(result > 0) {
+			System.out.println("new row inserted");
+		}
 	}
 
 }
